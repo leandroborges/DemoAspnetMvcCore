d2c9234e4ea6cc856d6bcf941a34c7e76d080e6b
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-09-28 16:36:32 -0300

    feat: rotas de prefixo exclusivo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace DemoAspnetMvcCore.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -18,17 +20,24 @@ namespace DemoAspnetMvcCore.Controllers
             _logger = logger;
         }
 
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id}")]
+        [Route("pagina-inicial/{id}/{categoria?}")]
         public IActionResult Index(string id, string categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
