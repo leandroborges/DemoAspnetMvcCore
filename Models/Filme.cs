e7d7600f17a33f6bbd9071f3b4512b97b4e8e605
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-09-28 16:57:40 -0300

    feat: criação da model

---
+++ Models/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoAspnetMvcCore.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Valor { get; set; }
+        public string Avaliacao { get; set; }
+    }
+}
