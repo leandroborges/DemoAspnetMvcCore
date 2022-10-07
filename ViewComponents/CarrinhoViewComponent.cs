77eb35d8b66948375df04347387d19b501edd6d0
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-10-07 15:47:42 -0300

    feat: add Viewccomponent

---
+++ ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace DemoAspnetMvcCore.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinho = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
