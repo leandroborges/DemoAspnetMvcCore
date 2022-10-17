984b0657878c485fbc9e3af57be0c0a029d88813
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-10-17 11:16:17 -0300

    feat: working with forms

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAspnetMvcCore.Controllers
+{
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public  IActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
