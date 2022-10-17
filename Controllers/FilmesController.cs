150a102e98dce82cd2e4962340b864b44243a1fd
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-10-17 11:34:57 -0300

    feat: forms validation

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoAspnetMvcCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DemoAspnetMvcCore.Controllers
 {
@@ -8,6 +9,17 @@ namespace DemoAspnetMvcCore.Controllers
         public  IActionResult Adicionar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(filme);
         }
     }
 }
