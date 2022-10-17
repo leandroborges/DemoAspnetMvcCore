aaee74c6fdec53a7f505942348067c88834f2225
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-10-17 12:44:56 -0300

    feat: using scafolld

---
+++ Data/DemoAspnetMvcCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoAspnetMvcCore.Models;
+
+namespace DemoAspnetMvcCore.Data
+{
+    public class DemoAspnetMvcCoreContext : DbContext
+    {
+        public DemoAspnetMvcCoreContext (DbContextOptions<DemoAspnetMvcCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoAspnetMvcCore.Models.Filme> Filme { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoAspnetMvcCore.Data;
 
 namespace DemoAspnetMvcCore
 {
@@ -24,6 +26,9 @@ namespace DemoAspnetMvcCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<DemoAspnetMvcCoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DemoAspnetMvcCoreContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
